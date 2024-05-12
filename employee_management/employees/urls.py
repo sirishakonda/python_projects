2a6ea0ff850e02422a738d29ba502145805c7c78
sirishakonda <93174840+sirishakonda@users.noreply.github.com>
2024-05-13 00:02:11 +0530

    Update urls.py

--- employee_management/employees/urls.py
+++ employee_management/employees/urls.py
@@ -2,6 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
+    path('', views.user_login, name='user_login'),
     path('login/', views.user_login, name='user_login'),
     path('logout/', views.user_logout, name='user_logout'),
     path('register/', views.user_register, name='user_register'),
